750cac4dd46bfd461a54a3f31cccc3a96c52516b
bhanuprasad2607 <bhanuprasad2607@github.com>
2021-07-29 22:11:48 +0530

    added datatypes

---
+++ data-types/datatypes.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main()
+{
+    int a = 10;
+    float b = 32.23;
+    char c = 'a';
+
+    std::cout << "Integer type of Data type :" << a << std::endl;
+    std::cout << "Float type of Data type :" << b << std::endl;
+    std::cout << "character type of Data type :" << c << std::endl;
+    return 0;
+}
